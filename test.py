608f0dce4ec37a138215386929f3f5b1573e5f90
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-18 10:56:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+print('i Start')
 print('  test.py for CircleCI')
 from core.lib.emulator import *
-print('i import')
+print('i from core.lib.emulator import *')
 end(file,source)
-print('i end')
+print('i end(file,source)')
+print('i Success!')
+print('i End')
