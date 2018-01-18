ca7c5f184693d5d762edeb1fe2490ef8a2bee132
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-18 16:19:42 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 print('i Start')
 print('  test.py for CircleCI')
 from core.lib.emulator import *
+from sys import argv
 print('i from core.lib.emulator import *')
+if source != open(sys.argv[1],'r').read() or file != open(sys.argv[1],'w') or path != sys.argv[1] or options != sys.argv[2:]:
+  exit(1)
+
+print('i Processing...')
 end(file,source)
 print('i end(file,source)')
 print('i Success!')
