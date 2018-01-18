f5dd3fbf0566e3e0efbe5f61eca57f0c8a93eeac
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-18 07:35:43 -0500

    Update emulator.py

--- core/lib/emulator.py
+++ core/lib/emulator.py
@@ -6,7 +6,13 @@ path = sys.argv[1]
 encoding = 'utf-8'
 if len(sys.argv) >= 3 and sys.argv[2] != 'utf-8':
     encoding = sys.argv[2]
+file = open(path,'r')
+source = file.read()
+file.close()
+del file
 file = open(path,'w')
-source = open(path,'r').read()
 lines = source.split('\n')
 options = sys.argv[3:]
+def end(file,source):
+    file.write(source)
+    file.close()
