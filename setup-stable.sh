e0354f035465f238d7d6d8bdae6aa84ba4235a5e
python3lover <35400564+python3lover@users.noreply.github.com>
2018-01-19 13:47:26 -0500

    Update setup-stable.sh

--- setup-stable.sh
+++ setup-stable.sh
@@ -1,13 +1,13 @@
 echo "  AWK.py 1.0.1 Stable .sh Installer"
 echo "i Install AWK.py 1.0.1 Stable"
 echo "i Download Directory"
-mkdir -p ~/Apps/awk-py/1.0.1
+mkdir -p ~/Apps/AWK.py/1.0.1
 echo "|-Done"
 echo "i Change working directory to ~/Apps/awk-py/1.0.1"
-cd ~/Apps/awk-py/1.0.1
+cd ~/Apps/AWK.py/1.0.1
 echo "|-Done"
 echo "i Get .tar.gz Archive"
-wget https://github.com/python3lover/awk-py/archive/1.0.1.tar.gz
+wget https://github.com/python3lover/AWK.py/archive/1.0.1.tar.gz
 echo "|-Done"
 echo "i Extract .tar.gz"
 tar -xvf 1.0.1.tar.gz
